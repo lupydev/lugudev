33bcfeea12b7368abb9d42fb1a51fe7f7439adf7
Luis Guzman <lumigu.dev@gmail.com>
2024-11-20 23:34:24 -0500

    ✨ Add modules

--- lugudev/components/__init__.py
+++ lugudev/components/__init__.py
@@ -2,5 +2,7 @@ from .description import description
 from .cv import cv
 from .external import external
 from .title import title
-from .experience_card import experience_card
+from .experience_card import experience_card, experience_card_mobile
 from .bullet_point import bullet
+from .project import project_card_left, project_card_right
+from .navbar import navbar_icons
